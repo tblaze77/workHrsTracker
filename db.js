85938bd13deafd26dbe6f20c43cf40bafd21c289
tblaze77 <toni.blazevic77@gmail.com>
2022-01-25 17:55:06 +0100

    basic config

---
+++ db.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+
+const connectDB = async () => {
+  try{
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+
+    });
+    console.log('mongo je povezan')
+  } catch(err){
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Work Hrs tracker app')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/post', (req, res) => {
+  console.log(req.body)
 })
+
+app.post('/employee',(req,res) => {
+    employeeCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error));
+  })
+
+  const connectDB = require('./db');
+
+  dotenv.config({path: './config/config.env'});
+
+  connectDB();
+
+  app.listen(3000, () => {
+    console.log(`Example app listening on port 3000`)
+  })
+
